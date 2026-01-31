a95f4f3831bfcf183549827e15a6956a87657c1f
Aldo-26 <aldo.vergara.a@uni.pe>
2026-01-31 17:23:24 -0500

    Refactor suma function and improve main logic

--- PC3/Problema01.cpp
+++ PC3/Problema01.cpp
@@ -1,27 +1,40 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void suma(char* p, int k){
+void suma(char* p, int k) {
+    int valores[10]; 
+    int n = 0;
+    
+    char* aux = p;
+    while (*aux != '\0') {
+        if (*aux >= '0' && *aux <= '9') {
+            int num = 0;
 
-    for(int i=0; i<5; i++){
-        for(int j=0; j<5; j++){
+            while (*aux >= '0' && *aux <= '9') {
+                num = (num * 10) + (*aux - '0');
+                aux++;
+            }
+            valores[n] = num;
+            n++;
+        } else {
+            aux++; 
+        }
+    }
 
-            if((*(p+i) + *(p+j)) == '15'){
-
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (valores[i] + valores[j] == k) {
+                cout << "Encontrado: " << valores[i] << " + " << valores[j] << " = " << k << endl;
             }
         }
     }
 }
 
-
-int main(){
-
-    char texto[] = "1,2,4,7,11";
-    char*p = texto;
-
-    cout<<*p + *(p+4);
-
+int main() {
+    char texto[] = "1,2,4,7,11"; 
     int k = 15; 
+
+    suma(texto, k);
 
     return 0;
 }
